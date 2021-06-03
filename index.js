df3e5fd92296c4463477528472197f1e7afd3a69
nickcabuno <nickcabuno17@gmail.com>
2021-06-03 13:51:36 -0400

    new message

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
+const cors = require('cors')
+
 const server = express()
 
+server.use(express.json())
+server.use(cors())
+server.use(express.static(path.join(__dirname, 'client/build')))
+
 if (process.env.NODE_ENV === 'production') {
-    console.log('code is deployed :D')
+    console.log('code is deployed :')
 }
 
 const PORT = process.env.PORT || 5000
 
 server.get('/api', (req, res) => {
-    res.json({message: `${COHORT} sucks lmao`})
+    res.json({message: `${process.env.COHORT} sucks lmao`})
 })
 
 server.use((req, res) => {
