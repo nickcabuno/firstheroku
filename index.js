3189598327de808073a3ed461866bf7335c71978
nickcabuno <nickcabuno17@gmail.com>
2021-06-03 13:19:48 -0400

    .

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('yessssuh')
+require('dotenv').config()
+const express = require('express')
+const server = express()
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('code is deployed :D')
+}
+
+const PORT = process.env.PORT || 5000
+
+server.get('/api', (req, res) => {
+    res.json({message: `${process.env.COHORT} ROCKS`})
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
