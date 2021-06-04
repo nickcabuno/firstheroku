50b077903e37903991c97b37fdbcfb9e2bfe1f57
nickcabuno <nickcabuno17@gmail.com>
2021-06-04 03:19:17 -0400

    yoooo

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 require('dotenv').config()
 const express = require('express')
-const path = require('path')
 const cors = require('cors')
 
 const server = express()
 
 server.use(express.json())
 server.use(cors())
-server.use(express.static(path.join(__dirname, 'client/build')))
-
-if (process.env.NODE_ENV === 'production') {
-    console.log('code is deployed :')
-}
 
 const PORT = process.env.PORT || 5000
 
-server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-})
-
 server.get('/api', (req, res) => {
-    res.json({message: `${process.env.COHORT} sucks lmao`})
+    res.json({ message: `${process.env.WHALE} sucks lmao` })
 })
 
-server.use((req, res) => {
-    res.status(404).json({message: "idk bruh"})
+server.use('*', (req, res) => {
+    res.send(`<h1>yoooooooo</h1>`)
+})
+
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
 })
 
 server.listen(PORT, () => {
-    console.log(`listening on ${PORT} `)
+    console.log(`${process.env.USERNAME} is listening on ${PORT} `)
 })
