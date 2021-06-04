7ffedbfa1f047bb8a8dc7a5cca1e60bbb8392768
nickcabuno <nickcabuno17@gmail.com>
2021-06-04 03:21:27 -0400

    belugas

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ server.use(cors())
 const PORT = process.env.PORT || 5000
 
 server.get('/api', (req, res) => {
-    res.json({ message: `${process.env.WHALE} sucks lmao` })
+    res.json({ message: `${process.env.WHALE} have teeth but refuse to chew :D` })
 })
 
 server.use('*', (req, res) => {
